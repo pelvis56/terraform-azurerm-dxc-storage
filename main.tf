1452ba7a913e413e0791b6440e8b5264817200bd
pelvis56 <106154146+pelvis56@users.noreply.github.com>
2022-06-10 14:47:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_template_deployment" "storage_account_arm" {
   name                = var.storage_account_name
   resource_group_name = var.resource_group_name
   deployment_mode     = "Incremental"
-  template_body       = file("storage-containers.json",)
+  template_body       = file("storage-containers.json")
   parameters = {
     storage_account_name    = var.storage_account_name
     location                = var.location

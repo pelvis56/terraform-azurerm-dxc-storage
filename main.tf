5ad0ffa5f724b5729d7f0eb98e9bc3221bd3ed19
pelvis56 <106154146+pelvis56@users.noreply.github.com>
2022-06-10 17:31:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,5 @@ resource "azurerm_template_deployment" "storage_account_arm" {
   parameters = {
     storage_account_name          = var.storage_account_name
     location                      = var.location
-    tls_version                   = var.tls_version
   }
 }

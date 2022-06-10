ddedb168cd164a6ea143f9d4f49b569b7d9d1a3e
pelvis56 <106154146+pelvis56@users.noreply.github.com>
2022-06-10 11:56:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ resource "azurerm_template_deployment" "stdstorage-containers" {
     "azurerm_storage_account.standard-storage"
   ]
  
-  template_body       = file("${path.module}/storage-containers.json")
-  parameters = {
-    storageAccountName      = azurerm_storage_account.standard-storage.name
-    location                = azurerm_storage_account.standard-storage.location   
+  parameters {
+    location           = "${var.location}"
+    storageAccountName = "${azurerm_storage_account.standard-storage.name}"
   }
-  
+
+  template_body = "${file("${path.module}/storage-containers.json")}"  
 }

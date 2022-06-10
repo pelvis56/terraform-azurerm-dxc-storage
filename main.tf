55390987b464adc6fb02161a490599b877174f78
pelvis56 <106154146+pelvis56@users.noreply.github.com>
2022-06-10 16:14:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "azurerm_storage_account" "standard-storage" {
   name                      = "${var.storage_account_name}"
   location                  = "${var.location}"
   resource_group_name       = "${var.resource_group_name}"
+  MinimumTlsVersion         = "${var.tls_version}"
   account_tier              = "Standard"
   account_replication_type  = "${var.standard_replication_type}"
   enable_https_traffic_only = true
@@ -14,8 +15,6 @@ resource "azurerm_template_deployment" "storage_account_arm" {
   template_body       = file("storage-containers.json")
   parameters = {
     storage_account_name          = var.storage_account_name
-    standard_replication_type     = var.standard_replication_type
-    tls_version                   = var.tls_version
     location                      = var.location
     StorageAccountKind            = "StorageV2"
   }

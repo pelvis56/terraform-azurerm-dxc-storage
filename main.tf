866c7e6a40ed0adc4892811c79eff2db5f77d671
pelvis56 <106154146+pelvis56@users.noreply.github.com>
2022-06-10 14:58:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-resource "azurerm_storage_account" "standard-storage" {
-  name                = "${var.storage_account_name}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
-
-  account_tier              = "Standard"
-  account_replication_type  = "${var.standard_replication_type}"
-  enable_https_traffic_only = true
-
-}
 
 resource "azurerm_template_deployment" "storage_account_arm" {
   name                = var.storage_account_name
@@ -15,8 +5,10 @@ resource "azurerm_template_deployment" "storage_account_arm" {
   deployment_mode     = "Incremental"
   template_body       = file("storage-containers.json")
   parameters = {
-    storage_account_name    = var.storage_account_name
-    location                = var.location
-    StorageAccountKind      = "StorageV2"
+    storage_account_name          = var.storage_account_name
+    standard_replication_type     = var.standard_replication_type
+    tls_version                   = var.tls_version
+    location                      = var.location
+    StorageAccountKind            = "StorageV2"
   }
 }

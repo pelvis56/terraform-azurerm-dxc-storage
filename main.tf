a47a219f73cebc1e54f8b9948eb774dbdfb033aa
pelvis56 <106154146+pelvis56@users.noreply.github.com>
2022-06-10 14:29:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,8 +16,6 @@ resource "azurerm_template_deployment" "storage_account_arm" {
   template_body       = file("storage-containers.json",)
   parameters = {
     storage_account_name    = var.storage_account_name
-    standard_replication_type    = var.standard_replication_type
-    tls_version    = var.tls_version
     location                = var.location
     StorageAccountKind      = "StorageV2"
   }
